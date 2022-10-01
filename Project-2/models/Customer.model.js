30e159a6267908094c9c2689a110d5e4b23a024c
Mitchell Diaz <mitchelldiazlmt@gmail.com>
2022-10-01 16:16:26 -0400

    added routes files for customer and purchase

--- Project-2/models/Customer.model.js
+++ Project-2/models/Customer.model.js
@@ -1,1 +1,14 @@
-const { Schema, model } = {}
+const { Schema, model } = mongoose;
+const mongoose = require('mongoose');
+
+
+const customerSchema = new Schema({
+    name: String,
+    occupation: String,
+    catchPhrase: String
+}, { 
+    timestamps: true
+})
+
+const Customer = model('Customer', customerSchema);
+module.exports = Customer;
