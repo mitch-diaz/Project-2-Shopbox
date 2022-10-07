300ed6694f4b6d675e06a4ace30ca6193497add7
zacjjohnson <wecooked@gmail.com>
2022-10-06 22:06:58 -0400

    added routes and views for books and movies. create book working

--- Project-2/app.js
+++ Project-2/app.js
@@ -37,6 +37,9 @@ app.use("/", index);
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
+// const bookRoutes = );
+app.use("/", require("./routes/book.routes"))
+app.use("/", require("./routes/movie.routes"))
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
--- Project-2/models/Movie.model.js
+++ Project-2/models/Movie.model.js
@@ -1,1 +1,24 @@
-const { Schema, model } = {}
+const { Schema, model } = require("mongoose");
+
+
+const movieSchema = new Schema(
+    {
+        title: String,
+        director: String,
+        runtime: String,
+        genre: String,
+        price: String,
+        inventory: String,
+        relatedBooks: {
+            type: Schema.Types.ObjectId,
+            ref: 'Book'
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Movie = model('Movie', movieSchema);
+
+module.exports = Movie;
